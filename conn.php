5573b3c766b4bdf35a5303357b2a5c526afe8398
Vipul Kumar <66182594+Vipul208@users.noreply.github.com>
2021-12-21 13:17:54 +0530

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,26 @@
+<?php
+$username = $_POST['username'];
+$email = $_POST['email'];
+$messag = $_POST['messag'];
+
+if(!empty($username)){
+	//create connection
+	$con = new mysqli('localhost', 'id15704794_root', 'Radheshyam@208', 'id15704794_dscproject');
+	if (mysqli_connect_error()) {
+	die('Connect Error('.mysqli_connect_error().')'. mysqli_connect_error());
+} else {
+	$sql = "INSERT INTO register (username, email, messag ) values('$username', '$email', '$messag')";
+if($con->query($sql)){
+    header("Location: data.html#contact");
+    exit();
+}
+else{
+  echo "Error".$sql."<br>".$conn->error;
+}
+$con->close();
+}
+}else {
+	echo "username should not be empty";
+	die();
+}
+?>
